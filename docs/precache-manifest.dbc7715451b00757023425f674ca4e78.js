28ee1e9d7f47f91e5032f6fc3f1378de2a99cfec
Diego <1.5.dg.diego@gmail.com>
2020-08-23 17:44:42 -0500

    Docs agregados

---
+++ docs/precache-manifest.dbc7715451b00757023425f674ca4e78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc473b6563b6b548c023",
+    "url": "/static/css/main.00d29525.chunk.css"
+  },
+  {
+    "revision": "cc473b6563b6b548c023",
+    "url": "/static/js/main.6057a088.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be1f3b286b1978f3e96f",
+    "url": "/static/js/2.308b9f92.chunk.js"
+  },
+  {
+    "revision": "031415bfe9f02e2fca0db1a6b75d2396",
+    "url": "/index.html"
+  }
+];
